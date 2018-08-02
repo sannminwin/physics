498306d93127e5e2d3d768807909b88753fa1dc5
nayyaung9 <myanmarphysicsrefwebsite@gmail.com>
2018-08-02 16:23:27 -0700

    edited and added

---
+++ note/grade-9/framework/chapter-4-forces.js
@@ -0,0 +1,28 @@
+
+
+$("document").ready(function(){
+    $("#answer-btn1").on("click", function(){
+        $(".answer1").fadeToggle();
+    });
+    $("#answer-btn2").on("click", function(){
+        $(".answer2").fadeToggle();
+    });
+    $("#answer-btn3").on("click", function(){
+        $(".answer3").fadeToggle();
+    });
+    $("#answer-btn4").on("click", function(){
+        $(".answer4").fadeToggle();
+    });
+    $("#answer-btn5").on("click", function(){
+        $(".answer5").fadeToggle();
+    });
+    $("#answer-btn6").on("click", function(){
+        $(".answer6").fadeToggle();
+    });
+    $("#answer-btn7").on("click", function(){
+        $(".answer7").fadeToggle();
+    });
+    $("#answer-btn8").on("click", function(){
+        $(".answer8").fadeToggle();
+    });
+}); 
